d3db35f1724cb33e1cf32766c0bd2430b093b33b
YoungHwal.Kim <89236406+younghwalkim@users.noreply.github.com>
2021-09-10 05:55:07 +0000

    캐비넷 if 문 제거

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from study import *
 
 
 # 케비넷관리
-userInfo = {"유재석","하하","김태호","김종국","조세호"}
-cabinetInfo = {"A":3,"B":5}
+userInfo = ["유재석","하하","김태호","김종국","조세호"]
+cabinetInfo = {"A":4,"B":4}
 
-Cabinet.cabinetAdmin(userInfo, cabinetInfo)
+cabinetResult = Cabinet.cabinetAdmin(userInfo, cabinetInfo)
+
+Cabinet.cabinetWho(cabinetResult,"김태호")
 
 # 로또번호
 # Lotto.lotto(5)
--- study/cabinet.py
+++ study/cabinet.py
@@ -7,30 +7,41 @@ class Cabinet:
     # print(cabinetInfo)
 
     cabinet = {}
-    count = 1
+    k = 0
 
+    # 키배정
     for cabinetCode in cabinetInfo:
       
       for i in range(cabinetInfo[cabinetCode]):       
 
         kyeNum = cabinetCode + "_" + str(randint(1,cabinetInfo[cabinetCode]));
-        
-        # 중복제거
-        if kyeNum in cabinet:
+
+        while kyeNum in cabinet:
+          # 중복발생
           kyeNum = cabinetCode + "_" + str(randint(1,cabinetInfo[cabinetCode]));
-          print("중복")
+        else:            
+          # 키 발급
+          if k < len(userInfo): 
+            # print("발급 " + kyeNum)
+            cabinet[kyeNum] = userInfo[k]
+          else:
+            # 인원수 초과시 종료
+            print("발급 완료")
+            break;
 
-          while kyeNum not in cabinet:
-             cabinet[kyeNum] = "."
-        else: 
-          cabinet[kyeNum] = "."
+        k = k + 1
 
-        print(kyeNum)
-        print(count)
-        count = count + 1
+    # print(str(cabinet) + "\n")
+
+    return cabinet
+
+  def cabinetWho(cabinetInfo, username):
+    print(cabinetInfo)
+    print(username)
+        
         
 
 
     
-    print(cabinet)
+   
 
