d1a7dbc9a4fa3d466e98a86bbaf1aeaa7356ea56
YoungHwal.Kim <89236406+younghwalkim@users.noreply.github.com>
2021-09-10 06:18:28 +0000

    1. 조회기능 2. 미사용 캐비넷 확인

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from study import *
 
 # 케비넷관리
 userInfo = ["유재석","하하","김태호","김종국","조세호"]
-cabinetInfo = {"A":4,"B":4}
+cabinetInfo = {"A":2,"B":8}
 
 cabinetResult = Cabinet.cabinetAdmin(userInfo, cabinetInfo)
 
 Cabinet.cabinetWho(cabinetResult,"김태호")
+
+Cabinet.cabinetUnused(cabinetResult, cabinetInfo)
+
+
 
 # 로또번호
 # Lotto.lotto(5)
--- study/cabinet.py
+++ study/cabinet.py
@@ -25,21 +25,37 @@ class Cabinet:
             # print("발급 " + kyeNum)
             cabinet[kyeNum] = userInfo[k]
           else:
-            # 인원수 초과시 종료
-            print("발급 완료")
+            # 인원 초과시 발급 종료
+            # print("발급 완료"  + kyeNum + " \n")
             break;
 
         k = k + 1
-
-    # print(str(cabinet) + "\n")
+    print("발급 완료")
+    print(str(cabinet) + "\n")
 
     return cabinet
 
   def cabinetWho(cabinetInfo, username):
-    print(cabinetInfo)
-    print(username)
+    # print(cabinetInfo)
+    # print(username)
+
+    for i in cabinetInfo: 
+      if cabinetInfo[i] == username:
+        print(username + " 님의 캐비넷 번호는 "+ i + " 입니다. \n")
+
+  def cabinetUnused(cabinetResult, cabinetInfo):   
+    # print(cabinetInfo) 
+
+    UnusedKey = []
+
+    for cabinetCode in cabinetInfo:      
+      for i in range(cabinetInfo[cabinetCode]):   
+        kyeNum = cabinetCode + "_" + str(i+1);
         
-        
+        if kyeNum not in cabinetResult:
+          UnusedKey.append(kyeNum)
+    
+    print("미사용 캐비넷 번호" + str(UnusedKey) + " 입니다. \n")
 
 
     
